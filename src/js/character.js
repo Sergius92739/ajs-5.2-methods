e1c8af1110a7f04e06da12082b27f2a1ac5a4f99
Sergius92739 <sergem279@gmail.com>
2021-06-27 01:09:12 +0300

    listOfTypes fixed

--- src/js/character.js
+++ src/js/character.js
@@ -6,6 +6,7 @@
  * @throws выбрасывает ошибку, если имя игрока меньше 2 символов или болше 10
  * или если тип персонажа не существует
  */
+
 export default class Character {
   constructor(name, type) {
     if (typeof name !== 'string') {
@@ -14,9 +15,7 @@ export default class Character {
       throw new Error('The name must be between 2 and 10 characters!');
     }
 
-    const listOfTypes = ['Bowman', 'Swordsman', 'Magician', 'Deamon', 'Undead', 'Zombie'];
-
-    if (!listOfTypes.includes(type)) {
+    if (!Character.listOfTypes().includes(type)) {
       throw new Error('The type must be selected from the list of suggested types!');
     }
 
@@ -24,6 +23,10 @@ export default class Character {
     this.type = type;
     this.health = 100;
     this.level = 1;
+  }
+
+  static listOfTypes() {
+    return ['Bowman', 'Swordsman', 'Magician', 'Deamon', 'Undead', 'Zombie'];
   }
 
   levelUP() {
